4afdfb0f19f94f9dcb923862240e1d4f35139ab1
青梅煮茶 <942943001@qq.com>
2018-05-20 23:16:48 +0800

    20180520

---
+++ Spring/src/main/java/com/imooc/ioc/interfaces/Main.java
@@ -0,0 +1,12 @@
+package com.imooc.ioc.interfaces;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OneInterfaceImpl oif=new OneInterfaceImpl();
+		
+		oif.say("20180520");
+	}
+
+}
