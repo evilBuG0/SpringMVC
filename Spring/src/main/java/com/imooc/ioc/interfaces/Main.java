2b45e76677e061a0d2b47a2be02fa9e6eed98077
青梅煮茶 <942943001@qq.com>
2018-05-22 00:26:58 +0800

    20180522

--- Spring/src/main/java/com/imooc/ioc/interfaces/Main.java
+++ Spring/src/main/java/com/imooc/ioc/interfaces/Main.java
@@ -1,12 +1,15 @@
 package com.imooc.ioc.interfaces;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		OneInterfaceImpl oif=new OneInterfaceImpl();
-		
-		oif.say("20180520");
+		ApplicationContext ca=new ClassPathXmlApplicationContext("spring-ioc.xml");
+		OneInterfaceImpl oti= (OneInterfaceImpl) ca.getBean("oneInterface");
+		oti.say("20180521");
 	}
 
 }
