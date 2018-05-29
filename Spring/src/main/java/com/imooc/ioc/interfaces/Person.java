1d4d0209e7780dfeca367e230849fae5f8067464
青梅煮茶 <942943001@qq.com>
2018-05-29 23:16:35 +0800

    20180529

--- Spring/src/main/java/com/imooc/ioc/interfaces/Person.java
+++ Spring/src/main/java/com/imooc/ioc/interfaces/Person.java
@@ -1,36 +1,36 @@
 package com.imooc.ioc.interfaces;
-
+import java.util.*;
 public class Person {
 	private String name;
 	private int age;
-	private Car car;
+	private List<Car> car;
 	public String getName() {
 		return name;
 	}
 	public int getAge() {
 		return age;
 	}
-	public Car getCar() {
-		return car;
-	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
 	public void setAge(int age) {
 		this.age = age;
 	}
-	public void setCar(Car car) {
+	
+
+	public List<Car> getCar() {
+		return car;
+	}
+	public void setCar(List<Car> car) {
 		this.car = car;
 	}
-	public Person(String name, int age, Car car) {
+	
+	public Person(String name, int age, List<Car> car) {
 		super();
 		this.name = name;
 		this.age = age;
 		this.car = car;
-	}
-	@Override
-	public String toString() {
-		return "Person [name=" + name + ", age=" + age + ", car=" + car + "]";
 	}
 	public Person() {}
 }
